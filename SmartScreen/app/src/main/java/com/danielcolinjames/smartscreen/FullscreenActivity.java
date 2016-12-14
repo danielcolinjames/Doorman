f2038f199df6643c22482bdbaf1cebe5e21afbdc
danielcolinjames <danielcolinjames@gmail.com>
2016-12-14 13:12:52 -0500

    Initial commit
    Initialized project with basic setup. Currently, this app tells you when
    the next couple buses (of a single route tag) are coming to a particular
    stop (defined as a stopId=#### in a query string).

---
+++ SmartScreen/app/src/main/java/com/danielcolinjames/smartscreen/FullscreenActivity.java
@@ -0,0 +1,295 @@
+package com.danielcolinjames.smartscreen;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.util.Xml;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+
+    private class DownloadXmlTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                Log.i("url", urls[0]);
+                return loadXmlFromNetwork(urls[0]);
+            } catch (IOException e) {
+                //return getResources().getString(R.string.connection_error);
+                Log.e("IO Error", e.getMessage());
+                return "error1";
+            } catch (XmlPullParserException e) {
+                //return getResources().getString(R.string.xml_error);
+                //return "error2";
+                Log.e("XML error", e.getMessage());
+                //e.printStackTrace();
+                //Log.e("error", e.toString());
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        public void onPostExecute(String result) {
+            //setContentView(R.layout.activity_fullscreen);
+            // Displays the HTML string in the UI via a WebView
+            TextView textOutput = (TextView) findViewById(R.id.outputText);
+            textOutput.setText(result);
+
+            Log.i("INFO onPostExecute", result);
+            // myWebView.loadData(result, "text/html", null);
+        }
+    }
+
+    private String loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
+        InputStream stream = null;
+        // Instantiate the parser
+        PredictionParser predictionParser = new PredictionParser();
+        List<PredictionParser.Prediction> predictions = null;
+
+        try {
+            stream = downloadUrl(urlString);
+            //Log.i("stream", stream.toString());
+
+            predictions = predictionParser.parse(stream);
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+
+        String output = "";
+        for (PredictionParser.Prediction prediction : predictions) {
+            output += prediction.routeTag + ": ";
+            output += prediction.nextBusTimes + "\n";
+            Log.i("PREDICTION OUTPUT", String.valueOf(prediction.nextBusTimes));
+        }
+        //return htmlString.toString();
+        return output;
+    }
+
+    // Given a string representation of a URL, sets up a connection and gets
+// an input stream.
+    private InputStream downloadUrl(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Starts the query
+        conn.connect();
+        return conn.getInputStream();
+    }
+
+
+//    public static final String WIFI = "Wi-Fi";
+//    public static final String ANY = "Any";
+//    private static final String URL = "http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&stopId=3197";
+    private static final String URL = "http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&stopId=5164";
+
+//    // Whether there is a Wi-Fi connection.
+//    private static boolean wifiConnected = false;
+//    // Whether there is a mobile connection.
+//    private static boolean mobileConnected = false;
+//    // Whether the display should be refreshed.
+//    public static boolean refreshDisplay = true;
+//    public static String sPref = null;
+
+    // Uses AsyncTask to download the XML feed from stackoverflow.com.
+    public void loadPage() {
+//        if((sPref.equals(ANY)) && (wifiConnected || mobileConnected)) {
+//            new DownloadXmlTask().execute(URL);
+//        }
+//        else if ((sPref.equals(WIFI)) && (wifiConnected)) {
+            new DownloadXmlTask().execute(URL);
+//        } else {
+//            // show error
+//        }
+    }
+
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private static final int UI_ANIMATION_DELAY = 300;
+    private final Handler mHideHandler = new Handler();
+    private View mContentView;
+    private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            // Delayed removal of status and navigation bar
+
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    };
+
+
+
+
+
+
+
+
+
+    private View mControlsView;
+    private final Runnable mShowPart2Runnable = new Runnable() {
+        @Override
+        public void run() {
+            // Delayed display of UI elements
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.show();
+            }
+            mControlsView.setVisibility(View.VISIBLE);
+        }
+    };
+    private boolean mVisible;
+    private final Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hide();
+        }
+    };
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fullscreen);
+
+        mVisible = true;
+        mControlsView = findViewById(R.id.fullscreen_content_controls);
+        mContentView = findViewById(R.id.fullscreen_content);
+
+
+        // Set up the user interaction to manually show or hide the system UI.
+        mContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggle();
+            }
+        });
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+        loadPage();
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+    private void toggle() {
+        if (mVisible) {
+            hide();
+        } else {
+            show();
+        }
+    }
+
+    private void hide() {
+        // Hide UI first
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        mControlsView.setVisibility(View.GONE);
+        mVisible = false;
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable);
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    @SuppressLint("InlinedApi")
+    private void show() {
+        // Show the system bar
+        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        mVisible = true;
+
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable);
+        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    /**
+     * Schedules a call to hide() in [delay] milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+}
---
+++ SmartScreen/app/src/main/java/com/danielcolinjames/smartscreen/PredictionParser.java
@@ -0,0 +1,232 @@
+package com.danielcolinjames.smartscreen;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.util.Xml;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Daniel on 2016-12-12.
+ */
+
+public class PredictionParser {
+
+    // Implementation of AsyncTask used to download XML feed from stackoverflow.com.
+
+    // ns = namespace which we aren't using
+    // because this page said not to https://developer.android.com/training/basics/network-ops/xml.html#choose
+    private static final String ns = null;
+
+
+    public List parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+
+//            try {
+//                URL url = new URL("http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=ttc&stopId=3197");
+//                parser.setInput(new InputStreamReader(getUrlData(url));
+//            } catch (Throwable t) {
+//                //Toast.makeText(t.toString(), Toast.LENGTH_LONG);
+//                Log.i("Info", "COULDN'T GET THE URL");
+//            }
+
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+            return readFeed(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+
+//    public class RetrieveFeed extends AsyncTask {
+//        protected Object doInBackground(Object[] objects) {
+//            //URL url = new URL("http://webservices.nextbus.com");
+//
+//
+////        }
+////        public InputStream getUrlData(String url) throws URISyntaxException, IOException {
+////            DefaultHttpClient client
+//            return url;
+//        }
+//    }
+
+    /*
+    <body copyright="All data copyright Toronto Transit Commission 2016.">
+        <predictions agencyTitle="Toronto Transit Commission" routeTitle="93-Parkview Hills" routeTag="93" stopTitle="Woodbine Ave At Barker Ave South Side" stopTag="9185">
+            <direction title="South - South - 93 Parkview Hills towards Woodbine Station">
+                <prediction epochTime="1481521876336" seconds="394" minutes="6" isDeparture="false" branch="93" dirTag="93_0_93" vehicle="7750" block="93_1_10" tripTag="33184263"/>
+                <prediction epochTime="1481522941011" seconds="1458" minutes="24" isDeparture="false" affectedByLayover="true" branch="93" dirTag="93_0_93" vehicle="7750" block="93_1_10" tripTag="33184264"/>
+                <prediction epochTime="1481524005686" seconds="2523" minutes="42" isDeparture="false" affectedByLayover="true" branch="93" dirTag="93_0_93" vehicle="7750" block="93_1_10" tripTag="33184265"/>
+            </direction>
+        </predictions>
+        <predictions agencyTitle="Toronto Transit Commission" routeTitle="91-Woodbine" routeTag="91" stopTitle="Woodbine Ave At Barker Ave South Side" stopTag="9185">
+            <direction title="South - South - 91 Woodbine towards Woodbine Station">
+                <prediction epochTime="1481522553254" seconds="1070" minutes="17" isDeparture="false" branch="91" dirTag="91_0_91C" vehicle="7765" block="91_1_10" tripTag="33184028"/>
+                <prediction epochTime="1481524017249" seconds="2534" minutes="42" isDeparture="false" affectedByLayover="true" branch="91" dirTag="91_0_91C" vehicle="8113" block="91_2_20" tripTag="33184029"/>
+            </direction>
+        </predictions>
+    </body>
+     */
+
+
+    private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List predictions = new ArrayList();
+
+        parser.require(XmlPullParser.START_TAG, ns, "body");
+        int count = 1;
+
+        while (parser.next() != XmlPullParser.END_TAG) {
+
+            //Log.d("DEBUG", String.valueOf(count));
+
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+
+            String name = parser.getName();
+            Log.d("DEBUG", "RFCOUNT = " + count + ", current tag = " + parser.getName());
+            count++;
+            Log.i("INFO", "readFeed, looking at this tag: " + name);// + parser.toString());
+            // starts by looking for the predictions tag
+            if (name.equals("predictions")) {
+
+                Log.i("INFO", "STOP NAME: " + parser.getAttributeValue(ns, "stopTitle"));
+                predictions.add(readPrediction(parser));
+                //
+
+            } else {
+                //skip(parser);
+                parser.nextTag();
+            }
+        }
+        for (int i = 0; i < predictions.size(); i++) {
+            Log.d("DEBUG", "predictions(" + i + ") = " + predictions.get(i).toString());
+        }
+        return predictions;
+    }
+
+    public static class Prediction {
+        public final String routeTag;
+        ArrayList<Integer> nextBusTimes;
+
+        private Prediction(String routeTag, ArrayList<Integer> nextBusTimes) {
+            this.routeTag = routeTag;
+            this.nextBusTimes = nextBusTimes;
+        }
+    }
+
+    private Prediction readPrediction(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "predictions");
+
+        String routeTag = "";
+        ArrayList<Integer> nextBusTimes = new ArrayList<Integer>();
+
+        //Log.i("depth", String.valueOf(parser.getDepth()));
+
+        int count = 1;
+
+        while (parser.next() != XmlPullParser.END_TAG) {
+            Log.i("INFO", "RPCount = " + count + ", Current tag being analyzed: " + parser.getName());
+            count++;
+
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                Log.d("DEBUG", "readPrediction: Skipping " + parser.getName() + " : " + parser.toString());
+                continue;
+            }
+
+            String tagName = parser.getName();
+
+            //Log.i("readPrediction", tagName);
+
+            if (tagName.equals("prediction")) {
+                routeTag = readRouteTag(parser);
+                Log.i("INFO", "branch = " + routeTag);
+//            } else if (tagName.equals("prediction")) {
+                int seconds = readSeconds(parser);
+                Log.i("INFO", "seconds = " + String.valueOf(seconds));
+                nextBusTimes.add(seconds);
+                //parser.nextTag();
+            }
+
+        }
+
+        for (int i = 0; i < nextBusTimes.size(); i++) {
+            Log.d("DEBUG", "NEXTBUSTIMES(" + i + "): " + nextBusTimes.get(i));
+        }
+
+        return new Prediction(routeTag, nextBusTimes);
+    }
+
+    //public static class readStop
+
+    private int readSeconds(XmlPullParser parser) throws XmlPullParserException, IOException {
+        // set seconds to -1 in case it can't parse it for some reason
+        int seconds = -1;
+
+        parser.require(XmlPullParser.START_TAG, ns, "prediction");
+        String tagName = parser.getName();
+
+        if (tagName.equals("prediction")) {
+            try {
+                seconds = Integer.parseInt(parser.getAttributeValue(null, "seconds"));
+            } catch (Exception e) {
+                Log.e("Error", "Couldn't parse the \"seconds\" attribute from the XML feed.");
+                Log.i("Error message given:", e.getMessage());
+            }
+            parser.nextTag();
+        }
+
+        //parser.require(XmlPullParser.END_TAG, ns, "prediction");
+        return seconds;
+    }
+
+    private String readRouteTag(XmlPullParser parser) throws XmlPullParserException, IOException {
+        String routeTag = "";
+
+        parser.require(XmlPullParser.START_TAG, ns, "prediction");
+        String tagName = parser.getName();
+
+        Log.i("INFO", "readRouteTag current tag = " + tagName);
+
+        if (tagName.equals("prediction")) {
+            routeTag = parser.getAttributeValue(null, "branch");
+            //parser.nextTag();
+        }
+
+        //parser.require(XmlPullParser.END_TAG, ns, "prediction");
+        return routeTag;
+    }
+
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalArgumentException();
+        }
+
+        int depth = 1;
+        while (depth != 0) {
+            //Log.i("current depth", String.valueOf(depth));
+
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+}
